0bf6c7a568a0b30df7b16e6a3fd295f2877fb4ce
Artem Vysochyn <artem.vysochyn@gmail.com>
2019-05-22 10:11:33 +0300

    Removed unused method in ServiceMessageCodec

--- services-gateway-netty/src/main/java/io/scalecube/services/gateway/ServiceMessageCodec.java
+++ services-gateway-netty/src/main/java/io/scalecube/services/gateway/ServiceMessageCodec.java
@@ -5,12 +5,10 @@ import io.netty.buffer.ByteBufAllocator;
 import io.netty.buffer.ByteBufInputStream;
 import io.netty.buffer.ByteBufOutputStream;
 import io.netty.buffer.Unpooled;
-import io.scalecube.services.api.ErrorData;
 import io.scalecube.services.api.ServiceMessage;
 import io.scalecube.services.exceptions.MessageCodecException;
 import io.scalecube.services.transport.api.DataCodec;
 import io.scalecube.services.transport.api.HeadersCodec;
-import java.lang.reflect.Type;
 import java.util.function.BiFunction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -96,37 +94,5 @@ public final class ServiceMessageCodec {
     }
 
     return builder.build();
-  }
-
-  /**
-   * Decode message.
-   *
-   * @param message the original message (with {@link ByteBuf} data)
-   * @param dataType the type of the data.
-   * @return a new Service message that upon {@link ServiceMessage#data()} returns the actual data
-   *     (of type data type)
-   * @throws MessageCodecException when decode fails
-   */
-  public static ServiceMessage decodeData(ServiceMessage message, Type dataType)
-      throws MessageCodecException {
-    if (dataType == null
-        || !message.hasData(ByteBuf.class)
-        || ((ByteBuf) message.data()).readableBytes() == 0) {
-      return message;
-    }
-
-    Object data;
-    Type targetType = message.isError() ? ErrorData.class : dataType;
-
-    ByteBuf dataBuffer = message.data();
-    try (ByteBufInputStream inputStream = new ByteBufInputStream(dataBuffer, true)) {
-      DataCodec dataCodec = DataCodec.getInstance(message.dataFormatOrDefault());
-      data = dataCodec.decode(inputStream, targetType);
-    } catch (Throwable ex) {
-      throw new MessageCodecException(
-          "Failed to decode data on message q=" + message.qualifier(), ex);
-    }
-
-    return ServiceMessage.from(message).data(data).build();
   }
 }
