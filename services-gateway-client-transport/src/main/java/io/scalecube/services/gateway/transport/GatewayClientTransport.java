1bc1ce737b0bf5d7d5c22e6dfcfd9c01139b2bca
Artem Vysochyn <artem.vysochyn@gmail.com>
2019-07-19 17:20:52 +0300

    Updated GatewayClientTransport.create method: get rid of executing clientBuilder from withing context of ClientTransport.create()  invocation

--- services-gateway-client-transport/src/main/java/io/scalecube/services/gateway/transport/GatewayClientTransport.java
+++ services-gateway-client-transport/src/main/java/io/scalecube/services/gateway/transport/GatewayClientTransport.java
@@ -3,28 +3,18 @@ package io.scalecube.services.gateway.transport;
 import io.scalecube.net.Address;
 import io.scalecube.services.transport.api.ClientChannel;
 import io.scalecube.services.transport.api.ClientTransport;
-import java.util.function.Function;
 
 /** Already knows which type of gateway client to create. */
 public class GatewayClientTransport implements ClientTransport {
 
-  private final GatewayClientSettings gatewayClientSettings;
-  private final Function<GatewayClientSettings, GatewayClient> clientBuilder;
+  private final GatewayClient gatewayClient;
 
-  GatewayClientTransport(
-      GatewayClientSettings gatewayClientSettings,
-      Function<GatewayClientSettings, GatewayClient> clientBuilder) {
-    this.gatewayClientSettings = gatewayClientSettings;
-    this.clientBuilder = clientBuilder;
+  GatewayClientTransport(GatewayClient gatewayClient) {
+    this.gatewayClient = gatewayClient;
   }
 
   @Override
   public ClientChannel create(Address address) {
-    GatewayClientSettings actualSettings =
-        GatewayClientSettings.from(gatewayClientSettings)
-            .host(address.host())
-            .port(address.port())
-            .build();
-    return new GatewayClientChannel(clientBuilder.apply(actualSettings));
+    return new GatewayClientChannel(gatewayClient);
   }
 }
