f1c171d3095861826d7edadc55b03d466612f541
segabriel <sergiy.gabriel@jivygroup.com>
2020-06-05 20:55:19 +0300

    Set original qualifier in cancel/complete cases and simplify their preparing

--- services-gateway-netty/src/main/java/io/scalecube/services/gateway/ws/GatewayMessages.java
+++ services-gateway-netty/src/main/java/io/scalecube/services/gateway/ws/GatewayMessages.java
@@ -1,9 +1,7 @@
 package io.scalecube.services.gateway.ws;
 
 import io.scalecube.services.api.ServiceMessage;
-import io.scalecube.services.api.ServiceMessage.Builder;
 import io.scalecube.services.exceptions.DefaultErrorMapper;
-import java.util.Optional;
 
 final class GatewayMessages {
 
@@ -18,42 +16,46 @@ final class GatewayMessages {
     // Do not instantiate
   }
 
-  static ServiceMessage newCancelMessage(long sid) {
-    Builder builder = ServiceMessage.builder();
-    setSid(builder, sid);
-    setSignal(builder, Signal.CANCEL);
-    return builder.build();
+  static ServiceMessage newCancelMessage(long sid, String qualifier) {
+    return ServiceMessage.builder()
+        .qualifier(qualifier)
+        .header(STREAM_ID_FIELD, sid)
+        .header(SIGNAL_FIELD, Signal.CANCEL.code())
+        .build();
   }
 
   static ServiceMessage newErrorMessage(ServiceMessage message, Throwable th) {
     ServiceMessage.Builder builder =
-        ServiceMessage.from(DefaultErrorMapper.INSTANCE.toMessage(message.qualifier(), th));
-    Optional.ofNullable(GatewayMessages.getSidHeader(message))
-        .ifPresent(s -> GatewayMessages.setSid(builder, s));
-    GatewayMessages.setSignal(builder, Signal.ERROR);
-    return builder.build();
-  }
-
-  static ServiceMessage newCompleteMessage(ServiceMessage message) {
-    ServiceMessage.Builder builder = ServiceMessage.builder();
-    Optional.ofNullable(GatewayMessages.getSidHeader(message))
-        .ifPresent(s -> GatewayMessages.setSid(builder, s));
-    GatewayMessages.setSignal(builder, Signal.COMPLETE);
-    return builder.build();
-  }
-
-  static ServiceMessage newResponseMessage(
-      long sid, ServiceMessage message, boolean isErrorResponse) {
-    ServiceMessage.Builder builder = ServiceMessage.from(message);
-    GatewayMessages.setSid(builder, sid);
-    if (isErrorResponse) {
-      GatewayMessages.setSignal(builder, Signal.ERROR);
+        ServiceMessage.from(DefaultErrorMapper.INSTANCE.toMessage(message.qualifier(), th))
+            .header(SIGNAL_FIELD, Signal.ERROR.code());
+    String sid = message.header(STREAM_ID_FIELD);
+    if (sid != null) {
+      builder.header(STREAM_ID_FIELD, sid);
     }
     return builder.build();
   }
 
+  static ServiceMessage newCompleteMessage(long sid, String qualifier) {
+    return ServiceMessage.builder()
+        .qualifier(qualifier)
+        .header(STREAM_ID_FIELD, sid)
+        .header(SIGNAL_FIELD, Signal.COMPLETE.code())
+        .build();
+  }
+
+  static ServiceMessage newResponseMessage(
+      long sid, ServiceMessage message, boolean isErrorResponse) {
+    if (isErrorResponse) {
+      return ServiceMessage.from(message)
+          .header(STREAM_ID_FIELD, sid)
+          .header(SIGNAL_FIELD, Signal.ERROR.code())
+          .build();
+    }
+    return ServiceMessage.from(message).header(STREAM_ID_FIELD, sid).build();
+  }
+
   static ServiceMessage validateSid(ServiceMessage message) {
-    if (getSidHeader(message) == null) {
+    if (message.header(STREAM_ID_FIELD) == null) {
       throw WebsocketContextException.badRequest("sid is missing", message);
     } else {
       return message;
@@ -78,27 +80,11 @@ final class GatewayMessages {
   }
 
   static long getSid(ServiceMessage message) {
-    return Long.parseLong(getSidHeader(message));
-  }
-
-  static String getSidHeader(ServiceMessage message) {
-    return message.header(STREAM_ID_FIELD);
-  }
-
-  static ServiceMessage.Builder setSid(ServiceMessage.Builder builder, long sid) {
-    return builder.header(STREAM_ID_FIELD, sid);
-  }
-
-  static ServiceMessage.Builder setSid(ServiceMessage.Builder builder, String sid) {
-    return builder.header(STREAM_ID_FIELD, sid);
+    return Long.parseLong(message.header(STREAM_ID_FIELD));
   }
 
   static Signal getSignal(ServiceMessage message) {
     String header = message.header(SIGNAL_FIELD);
     return header != null ? Signal.from(Integer.parseInt(header)) : null;
-  }
-
-  static ServiceMessage.Builder setSignal(ServiceMessage.Builder builder, Signal signal) {
-    return builder.header(SIGNAL_FIELD, signal.code());
   }
 }
